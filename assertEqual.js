1af5ec545d798d8900b91d17cb775692786aba15
Porson <porsonlee@gmail.com>
2019-11-26 02:32:04 +0000

    Completed assertEqual assignment

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + JSON.stringify(actual) + " !== " + JSON.stringify(expected));
+  }
+  return;
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
