d4cabe9a484af87c263c15197ccc8fd3a002fa13
Porson <porsonlee@gmail.com>
2019-11-28 09:08:14 +0000

    pushed for now. need to revisit to debug

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+// const eqArrays = require('./eqArrays');
+
+// copied from eqArrays.js
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+  return;
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+
+// incomplete - revisit to debug (Nov 28, 2019)
