9f2506802226aa92a37536ff20dad1c8179175da
Porson <porsonlee@gmail.com>
2019-11-28 22:09:29 +0000

    completed task

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${(actual)} !== ${(expected)}`);
+  }
+  // return;
+};
+
+const countLetters = function(itemsToCount) {
+  let stringArr = itemsToCount.split(' ').join('');
+  const letterResult = {};
+  for (const letter of stringArr) {
+    if (letterResult[letter]) {
+      letterResult[letter] += 1;
+    } else letterResult[letter] = 1;
+  }
+  // console.log(letterResult);
+  return letterResult;
+};
+
+console.log(countLetters("lighthouse in the house"));
