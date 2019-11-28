c39f12eff722b3b4e9978b8179e94ec0f486fad3
Porson <porsonlee@gmail.com>
2019-11-28 08:50:56 +0000

    removed JSON.stringify

--- head.js
+++ head.js
@@ -3,7 +3,7 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${JSON.stringify(actual)} !== ${JSON.stringify(expected)}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${(actual)} !== ${(expected)}`);
   }
   return;
 };
