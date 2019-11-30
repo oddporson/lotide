2cb72345df24b5c6851f1485c3db20e7d9ce5472
Porson <porsonlee@gmail.com>
2019-11-30 03:47:33 +0000

    nothing was changed

--- eqArrays.js
+++ eqArrays.js
@@ -12,8 +12,8 @@ const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-  for(let i = 0; i < arr1.length; i++) {
-    if(arr1[i] !== arr2[i]) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
@@ -25,7 +25,7 @@ const eqArrays = function(arr1, arr2) {
 assertEqual("Lighthouse Labs", "Bootcamp",); // ==> false
 assertEqual(1, 1); // ==> true
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 
 eqArrays([1, 2, 3], [1, 2, 3]); // => true
 eqArrays([1, 2, 3], [3, 2, 1]); // => false
