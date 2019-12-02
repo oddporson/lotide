0cadfde8f44410efc6c463b70e98164ec58a5da4
Porson <porsonlee@gmail.com>
2019-12-02 22:35:07 +0000

    added eqArraysTest.js

---
+++ test/eqArraysTest.js
@@ -0,0 +1,14 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays')
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp",); // ==> false
+assertEqual(1, 1); // ==> true
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
