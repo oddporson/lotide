da08d901bb4a10fb571b6052693cbab44a4da624
Porson <porsonlee@gmail.com>
2019-12-02 21:42:17 +0000

    removed parenthesis

--- assertEqual.js
+++ assertEqual.js
@@ -3,7 +3,7 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${(actual)} !== ${(expected)}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
   // return;
 };
