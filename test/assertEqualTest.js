8ce17567a58cfce9377a3a5c5cb3061b39fa0303
Porson <porsonlee@gmail.com>
2019-12-02 21:41:06 +0000

    no changes made

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -5,3 +5,6 @@ const assertEqual = require('../assertEqual');
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
