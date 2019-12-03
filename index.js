88512374e1379a63f859d306c5b3983f203963d5
Porson <porsonlee@gmail.com>
2019-12-03 02:15:31 +0000

    applied object shorthand

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays')
 
 module.exports = {
-  head:         head,
-  tail:         tail,
-  middle:       middle,
-  assertEqual:  assertEqual,
-  eqArrays:     eqArrays,
+  head,
+  tail,
+  middle,
+  assertEqual,
+  eqArrays,
 };
