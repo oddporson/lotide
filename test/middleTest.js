21bc40ef11419477bfaccd6855535b9bd6b6f4b4
Porson <porsonlee@gmail.com>
2019-12-03 01:30:43 +0000

    refactored with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,19 @@
-const assertArraysEqual = require('../assertArraysEqual')
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1, 2]),[]);// => [2]
-assertArraysEqual(middle([1, 2, 3, 4]),[2,3]);
-assertArraysEqual(middle([2,3,5,4,6]),5);
+
+describe("middle", () => {
+  it("returns empty space", () => {
+    assert.deepEqual(middle([1, 2],[]),([]));
+  }); 
+  it("returns [2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]),([2,3]));
+  });
+  it("returns 5", () => {
+    assert.deepEqual(middle([2,3,5,4,6]),5);
+  });
+});
+
+// assertArraysEqual(middle([1, 2]),[]);// => [2]
+// assertArraysEqual(middle([1, 2, 3, 4]),[2,3]);
+// assertArraysEqual(middle([2,3,5,4,6]),5);
