798e5d379cb793d23c1560982eeb7ce51610500d
Porson <porsonlee@gmail.com>
2020-03-04 10:36:25 +0000

    for review

---
+++ head.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function (arr) {
+  let firstItem = arr[0]
+  return firstItem;
+}
+
+
+
+// Test Code
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
