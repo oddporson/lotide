d27c959a98112e56f4df6f06e04ed8fffe8b89fe
Porson <porsonlee@gmail.com>
2020-03-18 22:48:03 -0700

    added a function that compares the 2 arrays

--- eqArrays.js
+++ eqArrays.js
@@ -6,3 +6,26 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      // console.log('show me loop of arr1 -->', arr1[i]);
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true; 
+  }
+}
+
+
+
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should FAIL
