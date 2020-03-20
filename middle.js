6d06e6b63124c29ebd2485418a26dc4dc7437577
Porson <porsonlee@gmail.com>
2020-03-20 00:02:38 -0700

    need to update eqarrays function

--- middle.js
+++ middle.js
@@ -23,7 +23,6 @@ const assertArraysEqual = function(actual, expected) {
 // ACTUAL FUNCTION
 
 const middle = function(array) {
-
 }
 
 
@@ -31,13 +30,13 @@ const middle = function(array) {
 // TEST CODE
 
 // For arrays with one or two elements, there is no middle. Return an empty array.
-middle([1]) // => []
-middle([1, 2]) // => []
+assertArraysEqual(middle([1])) // => []
+assertArraysEqual(middle([1, 2])) // => []
 
 // For arrays with odd number of elements, an array containing a single middle element should be returned.
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
+// assertArraysEqual(middle([1, 2, 3])) // => [2]
+// assertArraysEqual(middle([1, 2, 3, 4, 5])) // => [3]
 
 // For arrays with an even number of elements, an array containing the two elements in the middle should be returned.
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+// assertArraysEqual(middle([1, 2, 3, 4])) // => [2, 3]
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
