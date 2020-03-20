37bd7eee5237b25ad2a44a82daed51f822e0fedb
Porson <porsonlee@gmail.com>
2020-03-19 22:13:44 -0700

    refactored, linted and removed eqArrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,7 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true; 
-  }
-}
-
 const assertArraysEqual = function(actual, expected) {
-  if (JSON.stringify(actual) === JSON.stringify(expected)) {
-    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
-  } else {
+    JSON.stringify(actual) === JSON.stringify(expected) ?
+    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`) : 
     console.log(`❌❌❌ Assertion Failed: "${actual}" !== "${expected}"`);
-  }
 };
 
 
