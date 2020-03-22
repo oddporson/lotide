8a36a602d924c202642e75029e1162b036743101
Porson <porsonlee@gmail.com>
2020-03-21 18:01:12 -0700

    removed comments and refactored

--- eqArrays.js
+++ eqArrays.js
@@ -7,20 +7,10 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function (arr1, arr2) {
-  console.log("arr1 non stringify", arr1);
-  console.log("arr1 -->", JSON.stringify(arr1));
-  console.log("arr2 non stringify", arr2);
-  console.log("arr2 -->", JSON.stringify(arr2));
   return JSON.stringify(arr1) === JSON.stringify(arr2);
 }
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
-
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should FAIL
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false;
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
