d930fffb7504181b168ea89d29fd292db890bfc7
Porson <porsonlee@gmail.com>
2021-11-14 15:18:07 -0800

    linted all

--- letterPositions.js
+++ letterPositions.js
@@ -1,52 +1,14 @@
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${(actual)} !== ${(expected)}`);
-  }
-  return;
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-const letterPositions = function(strSentence) {
+const letterPositions = function(sentence) {
   const results = {};
-  for (let i = 0; i < strSentence.length; i++) {
-    if (results[strSentence[i]]) {
-      results[strSentence[i]].push(i);
-    } else {
-      results[strSentence[i]] = [i];
+  for (let i = 0; i < sentence.length; i++) {
+    let char = sentence[i];
+    if (char !== " ") {
+      !Array.isArray(results[char])
+        ? (results[char] = [i])
+        : results[char].push(i);
     }
   }
-  // logic to update results here
   return results;
 };
 
-console.log(letterPositions("lighthouse in the house"));
-
-assertArraysEqual(letterPositions("hello").e, [1]);
-
-// expected output
-/* {
-  l: [0],
-  i: [1, 11],
-  g: [2],
-  h: [3, 5, 15, 18],
-  t: [4, 14],
-  o: [6, 19],
-  u: [7, 20],
-  s: [8, 21],
-  e: [9, 16, 22],
-  n: [12]
-} */
+module.exports = letterPositions;
