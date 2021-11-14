a5e0b4717ad7485ff7a7301e26555d59f770f9d1
Porson <porsonlee@gmail.com>
2021-11-13 22:36:32 -0800

    refactored

--- tail.js
+++ tail.js
@@ -1,21 +1,5 @@
-// FUNCTION IMPLEMENTATION
-
-const assertEqual = require('./assertEqual');
-
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${(actual)} !== ${(expected)}`);
-//   }
-//   return;
-// };
-
-const tail = function(array) {
-  let exceptFirst = array.slice(1); 
-  return exceptFirst;
+const tail = function (notFirstElement) {
+  return notFirstElement.slice(1);
 };
 
-
 module.exports = tail
-
