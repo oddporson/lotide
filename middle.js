5b6d77c430b26b59fb0848280a6b9c2c8609bf46
Porson <porsonlee@gmail.com>
2021-11-13 22:57:11 -0800

    re structuring and one test is not passed which needs to be fixed

--- middle.js
+++ middle.js
@@ -1,35 +1,15 @@
-const eqArrays = require('./eqArrays');
-
-
-let middle = function(arr) {
-  let middleIndex = Math.floor((arr.length-1)/2);
-  let arrOutput = [];
-  if(arr.length > 2){
-    if(arr.length % 2 === 0) {
-    arrOutput = arr.slice(middleIndex,middleIndex+2);
-    } else {
-      arrOutput = arr[middleIndex];
-    }
+const middle = function (array) {
+  const mid = array.length / 2;
+  if (array.length < 3) {
+    return [];
   }
-  return arrOutput;
+  if (array.length % 2 === 1) {
+    const index = Math.floor(mid);
+    return [array[index]];
+  } else {
+    return [array[mid - 1], array[mid]];
+  }
 };
 
 module.exports = middle
 
-
-
-
-// revisit to debug this code (Dec 2, 2019)
-
-// const middle = function(arr) {
-//   if (arr.length <= 2) {
-//     return [];
-//   } else if (arr.length % 2 === 0) {
-//     let firstMiddle = arr[Math.floor(arr.length / 2) - 1];
-//     let secondMiddle = arr[Math.floor(arr.length / 2)];
-//     return [firstMiddle, secondMiddle];
-//   } else {
-//     let middle = arr[Math.floor(arr.length / 2)];
-//     return [middle];
-//   }
-// };
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,15 +1,28 @@
 const assert = require('chai').assert;
-const middle = require('../middle');
+// const middle = require('../middle');
+const _ = require("../index")
 
 
 describe("middle", () => {
   it("returns empty space", () => {
-    assert.deepEqual(middle([1, 2],[]),([]));
+    assert.deepEqual(_.middle([1, 2],[]),([]));
   });
   it("returns [2, 3]", () => {
-    assert.deepEqual(middle([1, 2, 3, 4]),([2,3]));
+    assert.deepEqual(_.middle([1, 2, 3, 4]),([2,3]));
   });
   it("returns 5", () => {
-    assert.deepEqual(middle([2,3,5,4,6]),5);
+    assert.deepEqual(_.middle([2,3,5,4,6]),5);
   });
-});
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(_.middle([1, 2, 3]), [2]);
+  });
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(_.middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns [2, 3] for [1, 2, 3, 4]]", () => {
+    assert.deepEqual(_.middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(_.middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+})
+})
