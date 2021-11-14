2fe8ecf76f2aa2ecd972d4f7fdf2f37dbc6d3113
Porson <porsonlee@gmail.com>
2021-11-14 12:38:08 -0800

    linted all

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const head                = require('./head');
 const tail                = require('./tail');
 const middle              = require('./middle');
 const assertEqual         = require('./assertEqual');
-const eqArrays            = require('./eqArrays')
-const countLetters        = require('./countLetters')
-const assertArraysEqual   = require('./assertArraysEqual')
-const countOnly           = require('./countOnly')
+const eqArrays            = require('./eqArrays');
+const countLetters        = require('./countLetters');
+const assertArraysEqual   = require('./assertArraysEqual');
+const countOnly           = require('./countOnly');
 
 module.exports = {
   head,
