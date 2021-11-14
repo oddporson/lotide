dfc2a91eee5a57b7a8c9726737246e6bb42cc56e
Porson <porsonlee@gmail.com>
2021-11-14 12:33:19 -0800

    refactored and use chai for better testing

--- countOnly.js
+++ countOnly.js
@@ -1,4 +1,4 @@
-const assertEqual = require('./assertEqual')
+// const assertEqual = require('./assertEqual')
 
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
--- test/countOnlyTest.js
+++ test/countOnlyTest.js
@@ -1,20 +1,25 @@
 const _ = require('../index')
+const assert = require('chai').assert
 
-//Test Codes
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = _.countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
-
-_.assertEqual(result1["Jason"], 1);
-_.assertEqual(result1["Karima"], undefined);
-_.assertEqual(result1["Fang"], 2);
+describe("#countOnly", () => {
+  const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe"
+  ];
+  it("should return { Fang: 2, Jason: 1 } for { 'Jason': true, 'Karima': true, 'Fang': true }", () => {
+    assert.deepEqual(_.countOnly(firstNames, { 'Jason': true, 'Karima': true, 'Fang': true }), { Fang: 2, Jason: 1 });
+  });
+  it("should return { Kavith: 1, Agouhanna: 1 } for { 'Agouhanna': true, 'Kavith': true }", () => {
+    assert.deepEqual(_.countOnly(firstNames, { 'Kavith': true, 'Agouhanna': true }), {  Kavith: 1, Agouhanna: 1 });
+  });
+  it("should return {} for { 'Karima': true }", () => {
+    assert.deepEqual(_.countOnly(firstNames, { 'Karima': true }), {});
+  });
+})
