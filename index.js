7bd7cbb8f5e024f1e858488534a766e619b83504
Porson <porsonlee@gmail.com>
2021-11-13 22:35:52 -0800

    restructured

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // index.js
-const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
-const assertEqual = require('./assertEqual');
-const eqArrays = require('./eqArrays')
-const countLetters = require('./countLetters')
-const assertArraysEqual = require('./assertArraysEqual')
+const head                = require('./head');
+const tail                = require('./tail');
+const middle              = require('./middle');
+const assertEqual         = require('./assertEqual');
+const eqArrays            = require('./eqArrays')
+const countLetters        = require('./countLetters')
+const assertArraysEqual   = require('./assertArraysEqual')
 
 module.exports = {
   head,
