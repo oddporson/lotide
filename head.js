c617373610b4dabd2d242fe5dab2a102c2c52bd4
Porson <porsonlee@gmail.com>
2021-11-13 22:35:29 -0800

    refactored

--- head.js
+++ head.js
@@ -1,21 +1,3 @@
-// FUNCTION IMPLEMENTATION
+const head = (element) => element[0];
 
-const assertEqual = require('./assertEqual');
-
-// no longer have to use this below when we can now require this code from the file called assertEqual.js
-// const assertEqual = function(actual, expected) {
-  // if (actual === expected) {
-  //   console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  // } else {
-  //   console.log(`🛑🛑🛑 Assertion Failed: ${(actual)} !== ${(expected)}`);
-  // }
-  // return;
-// };
-
-const head = function(array) {
-  let first = array[0];
-  return first;
-};
-
-
-module.exports = head
+module.exports = head;
