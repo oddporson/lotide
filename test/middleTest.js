e7688c0a5f6728b90e7e4be877eb9e4b581d7c53
Porson <porsonlee@gmail.com>
2021-11-14 11:44:28 -0800

    removed one of the tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,5 @@
 const assert = require('chai').assert;
-// const middle = require('../middle');
 const _ = require("../index")
-
 
 describe("middle", () => {
   it("returns empty space", () => {
@@ -9,9 +7,6 @@ describe("middle", () => {
   });
   it("returns [2, 3]", () => {
     assert.deepEqual(_.middle([1, 2, 3, 4]),([2,3]));
-  });
-  it("returns 5", () => {
-    assert.deepEqual(_.middle([2,3,5,4,6]),5);
   });
   it("returns [2] for [1, 2, 3]", () => {
     assert.deepEqual(_.middle([1, 2, 3]), [2]);
@@ -24,5 +19,5 @@ describe("middle", () => {
   });
   it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
     assert.deepEqual(_.middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-})
+  })
 })
