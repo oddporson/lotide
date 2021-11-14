c9386cb84aad1d040a2029f83c40f4ffdff0b1e4
Porson <porsonlee@gmail.com>
2021-11-14 15:01:01 -0800

    updated to require tail from index.js entire library

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,12 @@
 const assert = require('chai').assert;
-const tail   = require('../tail');
-
+const _ = require("../index")
 
 describe("#tail", () => {
-  it("returns [\"Lighthouse\", \"Labs\"]", () => {
-    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(_.tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
   });
 });
 
-// TEST CODE
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
-
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+_.tail(words); // no need to capture the return value since we are not checking it
